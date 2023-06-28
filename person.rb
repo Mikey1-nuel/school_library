74e27ea4940c3116788c8c2fe5b0b720ec23a5a8
Emmanuel <emmanuelnwoye12@gmail.com>
2023-06-28 22:55:02 +0100

    Update Person class

--- person.rb
+++ person.rb
@@ -14,14 +14,13 @@ class Person < Nameable
     @parent_permission = parent_permission
   end
 
-  
   def can_use_services?
     of_age? || @parent_permission
     false
   end
-  
+
   private
-  
+
   def of_age?
     @age >= 18
   end
