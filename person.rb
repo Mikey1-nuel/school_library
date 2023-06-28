8328230bf057a2f9b3404fd0efe2626585979f3c
Emmanuel <emmanuelnwoye12@gmail.com>
2023-06-28 22:52:39 +0100

    Update Person class

--- person.rb
+++ person.rb
@@ -6,26 +6,36 @@ class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, parent_permission: true, name: 'Unknown')
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
-    @id = 0
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def correct_name
-    @name
-  end
-
+  
   def can_use_services?
     of_age? || @parent_permission
     false
   end
-
+  
   private
-
+  
   def of_age?
     @age >= 18
   end
+
+  public
+
+  def correct_name
+    @name
+  end
 end
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
