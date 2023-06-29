1f89ec4e144af8d8982dfb0afc0648c2c6ad09d4
Emmanuel <emmanuelnwoye12@gmail.com>
2023-06-29 19:57:03 +0100

    Update teacher file

--- teacher.rb
+++ teacher.rb
@@ -4,7 +4,7 @@ class Teacher < Person
   attr_accessor :specialization
 
   def initialize(age, specialization, parent_permission: true, name: 'Unknown')
-    super(age, name, parent_permission: parent_permission)
+    super(age, name: name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
