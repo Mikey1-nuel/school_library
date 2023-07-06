6b77edb670b81382d51e7f7619436f54bcc120e5
Emmanuel <emmanuelnwoye12@gmail.com>
2023-07-06 09:38:09 +0100

    Create first batch of unit test

---
+++ spec/base_decorator_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../capitalize_decorator'
+require_relative '../trimmer_decorator'
+require_relative '../person'
+
+describe 'Decorators' do
+  before :all do
+    @person = Person.new(22, 'maximilianus')
+  end
+
+  describe '#capitalize_decorator' do
+    it 'returns a capitalized name' do
+      capitalized_person = CapitalizeDecorator.new(@person)
+      expect(capitalized_person.correct_name).to eq 'Maximilianus'
+    end
+  end
+
+  describe '#trimmer_decorator' do
+    it 'returns a name trimmed to 10 characters long if was initially longer' do
+      trimmed_person = TrimmerDecorator.new(@person)
+      expect(trimmed_person.correct_name).to eq 'maximilian'
+    end
+  end
+
+  describe '#trimmer_decorator' do
+    it 'returns a name trimmed to 10 characters long if was initially longer' do
+      capitalized_person = CapitalizeDecorator.new(@person)
+      trimmed_person = TrimmerDecorator.new(capitalized_person)
+      expect(trimmed_person.correct_name).to eq 'Maximilian'
+    end
+  end
+end
